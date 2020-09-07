10c7c23b8e0baf43aebc0b0a7d53edab2386c2aa
204353 <69662795+204353@users.noreply.github.com>
2020-09-07 09:02:31 +0800

    Update as36.py

--- as36.py
+++ as36.py
@@ -2,3 +2,7 @@ numberlist = [1, 2, 3, 4, 5, 6, 7]
 print("The first number is:", numberlist[0])
 newlist = numberlist[::-1]
 print("The last number is:", newlist[0])
+newlist2 = []
+newlist2.append(numberlist[0])
+newlist2.append(newlist[0])
+print(newlist2)
