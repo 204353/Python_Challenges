b92c81a39519812cabf203b5430ebf12d9530ef4
204353 <69662795+204353@users.noreply.github.com>
2020-09-21 22:25:57 +0800

    Add files via upload

---
+++ gc39.py
@@ -0,0 +1,7 @@
+name = input(str("what is your name?"))
+
+import random
+
+insult = random.choice(["everyone’s entitled to act stupid once in awhile but you really abuse the privilege","were you born on the highway? That is where most accidents happen.","you always bring me so much joy—as soon as you leave the room.","do your parents even realize they’re living proof that two wrongs don’t make a right?"])
+
+print(name,",",insult)
