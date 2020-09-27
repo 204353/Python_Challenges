3ecbe1c8449b3e6d1bd906ce91f1ac96263cabae
204353 <69662795+204353@users.noreply.github.com>
2020-09-27 22:32:49 +0800

    Add files via upload

---
+++ as75.py
@@ -0,0 +1,16 @@
+def binaryconverter(int):
+    if binary > 1:
+
+while True:
+    try:
+        number = int(input("Input a number"))
+        break
+    except ValueError:
+        print("Not a number")
+bcdlist = []
+for i in str(number):
+    binary = int(i)
+    binary = bin(binary)[2:]
+    bcdlist.append(binary)
+string = ""
+print(f"the binary coded decimal for {number} is {string.join(bcdlist)}")
