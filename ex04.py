4904b71d494e47f8a79309f23c764eeb3a02aaa8
204353 <69662795+204353@users.noreply.github.com>
2020-10-06 21:03:49 +0800

    Update ex04.py

--- ex04.py
+++ ex04.py
@@ -11,15 +11,15 @@ def morse(txt):
                '1':'.----', '2':'..---', '3':'...--',
                '4':'....-', '5':'.....', '6':'-....',
                '7':'---...', '8':'---..', '9':'----.', '0':'-----', ' ':' '}
-    translator = {v: k for k, v in morsecode.items()} #this translates the user input into morse code 1 by 1
-    if '-' in txt or '.' in txt:
-        return ''.join(translator[i] for i in txt.split())#this looks to make sure if
-    return ' '.join(morsecode[i] for i in txt.upper())
+    translator = {v: k for k, v in morsecode.items()} #this makes a reversed dictionary if it needs to be used for morse code
+    if '-' in txt or '.' in txt: #this checks whether the input is morse code or letters
+        return ''.join(translator[i] for i in txt.split())#this will split up the morse code into a list
+    return ' '.join(morsecode[i] for i in txt.upper()) #this converts the string to uppercase to reduce the length of the dictionary
 while True:
     try:
         menu = int(input("Type 1 if you wish to use the translator, or type 2 if you want to exit the program:"))
         if (menu == 1):
-            inputs = input("Input what you would like to be converted. For morse code, please use spaces to designate between a letter\n:")
+            inputs = input("Input what you would like to be converted.\n:")
             print(morse(inputs))
             break
         elif (menu ==2):
