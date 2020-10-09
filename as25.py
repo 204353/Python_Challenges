5b4dab25b7b6850d8e9d07b63eea4043c8033e25
204353 <69662795+204353@users.noreply.github.com>
2020-10-09 19:09:14 +0800

    Update as25.py

--- as25.py
+++ as25.py
@@ -1,10 +1,4 @@
 def histogram( num ):
     for i in num:
-        output = ''
-        times = i
-        while( times > 0 ):
-          output += '*'
-          times = times - 1
-        print(output)
-
-histogram([4, 9, 7])
+        print('*' * i)
+histogram([9,8,7])
