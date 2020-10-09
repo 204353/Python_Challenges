c7689b4c860a2db1c3909908a648ff023d70d7de
204353 <69662795+204353@users.noreply.github.com>
2020-10-09 18:55:53 +0800

    Update a246.py

--- a246.py
+++ a246.py
@@ -1,20 +1,36 @@
 import random
 n = []
+c = n
 for i in range(1, 112):
     n.append(i)
+b = random.choice(n) #this is a constant please dont change this
 i = 0
+f = 0
+print("We are thinking of a number from 1 to 111. Can you guess what it is? We'll tell you if it is higher or lower.")
 while True:
-    x = random.choice(n)
-    print(x)
-    e = n.index(x)
-    a = input()
-    a = a.lower()
-    if a == 'higher':
-        del n[0:e+1]
-        i += 1
-    elif a == 'lower':
+    try:
+        a = int(input("Input a number between 1 and 111\n"))
+        e = n.index(a)
+        f += 1
+        if a > b:
+            print(f"The number is lower than {a}.")
+            del c[e::]
+        elif a < b:
+            print(f"The number is higher than {a}")
+            del c[0:e+1]
+        else:
+            print(f"Wow you got it. It only took you {f} tries")
+            break
+    except ValueError:
+        print("We already told you that that is not our number")
+while True:
+    a = random.choice(n)
+    e = n.index(a)
+    i += 1
+    if a > b:
         del n[e::]
-        i += 1
+    elif a < b:
+        del n[0:e+1]
     else:
-        print(f"It took {i} tries")
+        print(f"It took our cpu {i} tries to find our number.")
         break
