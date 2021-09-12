94f10b72bcef4cd718757005c1003f6b47f32bb9
204353 <69662795+204353@users.noreply.github.com>
2021-09-12 22:49:26 +0800

    Update ex04.py

--- ex04.py
+++ ex04.py
@@ -1,54 +1,49 @@
-morsecode = {'A':'.-', 'B':'-...', 'C':'-.-.',
-           'D':'-..', 'E':'.', 'F':'..-.',
-           'G':'--.', 'H':'....', 'I':'..',
-           'J':'.---', 'K':'-.-', 'L':'.-..',
-           'M':'--', 'N':'-.', 'O':'---',
-           'P':'.--.', 'Q':'--.-', 'R':'.-.',
-           'S':'...', 'T':'-', 'U':'..-',
-           'V':'...-', 'W':'.--', 'X':'-..-',
-           'Y':'-.--', 'Z':'--..', ':':'---...',
-           '1':'.----', '2':'..---', '3':'...--',
-           '4':'....-', '5':'.....', '6':'-....',
-           '7':'---...', '8':'---..', '9':'----.',
-           '0':'-----', ' ':' ', '.':'.-.-.-',
-           ',':'--..--', '?':'..--..', "'":".---.",
-           '!':'-.-.--', '/':'-..-.', '(':'-.--.',
-           ')':'-.--.-', '&':'.-...', ':': '---...',
-           ';':'-.-.-.', '=':'-...-', '+':'.-.-.',
-           '-': '-....-', '_':'..--.-', '"':'.-..-',
-           '$':'...-..-', '@': '.--.-.'}
-translator = {v: k for k, v in morsecode.items()} #this makes a reversed dictionary if it needs to be used for morse code
- #this converts the string to uppercase to reduce the length of the dictionary
-translator = {v: k for k, v in morsecode.items()} #this makes a reversed dictionary if it needs to be used for morse code
-def menu():
-    print("Press 1 to convert text to morse")
-    print("Press 2 to convert morse to text")
-    print("Press 3 to end")
+english_to_morsecode = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '-':'-....-','':'',
+                    '(':'-.--.', ')':'-.--.-', ' ':'/'}
+
+morsecode_to_english = {v:k for k,v in english_to_morsecode.items()}
+def eng_to_m (message):
+    translated = ''
+    for i in message:
+         translated += english_to_morsecode[i.upper()]
+         translated += ' '
+    print(translated)
+def m_to_eng (message): 
+    y = message.split(" ")
+    translated = ''
+    print(y)
+    for i in y:
+        if i == ' ':
+            pass
+        translated += morsecode_to_english[i]
+    print(translated)
+
 while True:
     try:
-        menu()
+        print("1 : English to morsecode translator \n2 : morsecode to English translator \n3 : QUIT")
         x = int(input())
         if x == 1:
-            try:
-                y = str(input("Input text:"))
-                v = ''
-                for i in y.upper():
-                    v += morsecode[i]
-                print(v)
-            except KeyError:
-                print("some part of that isn't text")
+            message = input("Input your message in English:\n")
+            eng_to_m(message)
         elif x == 2:
-            try:
-                y = str(input("Input morse:"))
-                v = ''
-                for i in y.split():
-                    v += translator[i]
-                print(v)
-            except KeyError:
-                print("some part of that isn't morse")
+            message = input("Input your message in Morsecode. Use /  for spaces")
+            m_to_eng(message)
         elif x == 3:
             break
         else:
-            print('invalid')
+            print("invalid, try again")
     except ValueError:
-        print('Value Error')
+        print("Error, invalid option")
